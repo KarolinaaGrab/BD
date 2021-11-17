c4aa50fc6a7f85ba9998f4f6dd8dc187025eb001
KarolinaaGrab <92163557+KarolinaaGrab@users.noreply.github.com>
2021-11-17 16:45:19 +0100

    Update ZADlab03.sql

--- ZADlab03.sql
+++ ZADlab03.sql
@@ -2,16 +2,17 @@
 
 CREATE TABLE postac (
 id_postaci int PRIMARY KEY AUTO_INCREMENT, 
-nazwa VARCHAR(40) not null, 
+nazwa VARCHAR(40), 
 rodzaj enum('wiking','ptak','kobieta'),
 data_ur date,
 wiek int(3) unsigned
 );
 
 INSERT INTO postac VALUES
-(1, 'Bjorn', 'wiking', '1945-12-12', 30),
-(2, 'Drozd', 'ptak', '1970-12-12', 5),
-(3, 'Tesciowa', 'kobieta', '1930-10-10', 45);
+(default, 'Bjorn', 'wiking', '1945-12-12', 30);
+INSERT INTO postac(nazwa, rodzaj, data_ur, wiek) VALUES
+('Drozd', 'ptak', '1970-12-12', 5),
+('Tesciowa', 'kobieta', '1930-10-10', 45);
 
 UPDATE postac SET wiek=88 WHERE nazwa='Tesciowa';
 
@@ -24,40 +25,44 @@ kolor enum('rozowy', 'teczowy', 'zolty'),
 id_wlasciciela int,
  FOREIGN KEY (id_wlasciciela) REFERENCES postac(id_postaci) on delete cascade);
 
-ALTER TABLE walizka ALTER kolor SET DEFAULT 'rozowy';
+ALTER TABLE walizka ALTER COLUMN kolor SET DEFAULT 'rozowy';
 
 INSERT INTO walizka VALUES
-(default, 15, 'zolty', 1),
-(default, 20, 'rozowy', 3);
+(default, 15, 'zolty', 1);
+INSERT INTO walizka (pojemnosc, kolor, id_wlasciciela) VALUES
+(20, 'rozowy', 3);
 
 # 3
 
 CREATE TABLE izba (
-adres_budynku VARCHAR(40) not null,
-nazwa_izby VARCHAR (40) not null,
+adres_budynku VARCHAR(40),
+nazwa_izby VARCHAR (40),
  PRIMARY KEY (adres_budynku, nazwa_izby),
 metraz int unsigned,
 wlasciciel int,
-FOREIGN KEY (wlasciciel) REFERENCES postac (id_postaci) on delete set null);
+FOREIGN KEY (wlasciciel) REFERENCES postac(id_postaci) on delete set null);
+
+ALTER TABLE izba ADD COLUMN kolor VARCHAR(40) default 'czarny' after metraz;
 
 INSERT INTO izba VALUES
-('sienkiewicza 3', 'spizarnia', 40, 1);
+('Sienkiewicza 3', 'spizarnia', default, 40, 1);
 
-ALTER TABLE izba ADD kolor VARCHAR(40) default 'czarny' after metraz;
+
 # 4
 
 CREATE TABLE przetwory (
 id_przetworu int PRIMARY KEY,
-rok_produkcji int(4) default '1654',
+rok_produkcji int(4) default 1654,
 id_wykonawcy int,
-FOREIGN KEY (id_wykonawcy) REFERENCES postac (id_postaci),
+FOREIGN KEY (id_wykonawcy) REFERENCES postac(id_postaci),
 zawartosc VARCHAR(40),
 dodatek VARCHAR(40) default 'papryczka chilli',
 id_konsumenta int,
-FOREIGN KEY (id_konsumenta) REFERENCES postac (id_postaci));
+FOREIGN KEY (id_konsumenta) REFERENCES postac(id_postaci)
+);
 
-INSERT INTO przetwory (id_przetworu, zawartosc) VALUES
-(1, 'bigos');
+INSERT INTO przetwory (id_przetworu, zawartosc, dodatek) VALUES
+(1, 'bigos', default);
 
 # 5
 
@@ -83,11 +88,10 @@ ALTER TABLE postac ADD COLUMN funkcja VARCHAR(40);
 UPDATE postac SET funkcja='kapitan' WHERE nazwa='Bjorn';
 
 ALTER TABLE postac ADD statek VARCHAR(40);
-ALTER TABLE postac ADD FOREIGN KEY(statek) REFERENCES statek(nazwa_statku);
-UPDATE postac SET statek='Ogien' WHERE nazwa 'Bjorn';
+ALTER TABLE postac ADD FOREIGN KEY (statek) REFERENCES statek(nazwa_statku);
 
-UPDATE postac SET statek='Ekipa' WHERE rodzaj='wiking';
-UPDATE postac SET statek='Ogien' WHERE nazwa='Bjorn';
+UPDATE postac SET statek='Ekipa' WHERE nazwa='Witold' and nazwa='Adam' and nazwa='Jonasz';
+UPDATE postac SET statek='Ogien' WHERE nazwa='Bjorn' and nazwa='Piotr'and nazwa='Jan';
 
 DELETE FROM izba WHERE nazwa_izby='spizarnia';
 DROP TABLE izba;
