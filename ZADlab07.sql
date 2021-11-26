6a4ff580258adaad8595bce5741f88485f473ea6
KarolinaaGrab <92163557+KarolinaaGrab@users.noreply.github.com>
2021-11-26 15:31:49 +0100

    Add files via upload

---
+++ ZADlab07.sql
@@ -0,0 +1,53 @@
+Zad 1 
+1)
+Jezeli pracuje na wikingowie nie trzeba
+
+2)
+select * from kreatura k
+inner join uczestnicy u on k.idKreatury=u.id_uczestnika;
+
+select distinct id_uczestnika from uczestnicy;
+select idKreatury from kreatura;
+
+(czyli 15 jest uczestnikami,a 4 nie)
+
+
+
+podzapytanie:
+select idKreatury from kreatura
+where idKreatury not in (select id_uczestnika from uczestnicy);
+ROZWIAZANIE:
+
+LEFT JOIN:
+left join - pokazalo null i wsz wyniki; teraz trzeba wstawic warunek where (...) null - wynik- postaci, kt nie brały udziału)
+
+select nazwa from kreatura k
+Left join uczestnicy u
+on k.idKreatury=u.id_uczestnika
+where u.id_uczestnika is null;
+
+3)
+select w.nazwa, sum(e.ilosc) from wyprawa w
+inner join uczestnicy u
+on w.id_wyprawy=u.id_wyprawy
+join ekwipunek e
+on u.id_uczestnika=e.idKreatury
+group by w.nazwa;
+
+#Zad2
+1)
+select rodzaj, count(*) from kreatura group by rodzaj;
+group_concat ---- wyswietla wsz wartosci, kt znalazly sie w tej grupie na podstawie nadanego wczesniej warunku grupujacego
+select rodzaj, group_concat(nazwa seperator '-') from kreatura group by rodzaj;
+
+rozwiazanie:
+select w.nazwa, count(u.id_uczestnika),group_concat(k.nazwa)
+from kreatura k
+inner join uczestnicy u
+on u.id_uczestnika=k.idKreatury
+inner join wyprawa w
+on w.id_wyprawy=u.id_wyprawy
+group by w.nazwa;
+
+2)
+
